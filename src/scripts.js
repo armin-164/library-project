1525ff46f8fedfec32116a9af4386bc0fa177b90
Armin Sadr <arminsadr164@hotmail.com>
2023-02-23 02:46:29 +0100

    Add some javascript code
    Add an empty array to act as a container for the objects (books)
    Add a constructor for the book objects
    Add a function that creates the elements for the book and appends them
    Add a function that goes through each every array object and call
    the createBookElements function on each book that creates them
    and appends them

---
+++ src/scripts.js
@@ -0,0 +1,62 @@
+const bookContainer = document.querySelector(".bookcontainer");
+
+const myLibrary = [
+  {
+    title: "The Hobbit",
+    author: "JRR Tolkien",
+    pages: 295,
+    readstatus: "Have read",
+  },
+  {
+    title: "The",
+    author: "Dr Dre",
+    pages: 295,
+    readstatus: "Have read",
+  },
+];
+
+
+function Book(title, author, pages, readstatus) {
+  this.title = `Title: ${title}`;
+  this.author = `Author: ${author}`;
+  this.pages = `Pages: ${pages}`;
+  this.readstatus = readstatus;
+
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${readstatus}`;
+  };
+}
+
+function createBookElements(book) {
+  const bookBox = document.createElement("div");
+  const bookBoxButtons = document.createElement("div");
+
+  bookBox.classList.add("book");
+  bookBoxButtons.classList.add("bookbuttons");
+
+  const firstP = document.createElement("p");
+  const secondP = document.createElement("p");
+  const thirdP = document.createElement("p");
+
+  firstP.innerText = book.title;
+  secondP.innerText = book.author;
+  thirdP.innerText = book.pages;
+
+  const statusbutton = document.createElement("button");
+  const removebutton = document.createElement("button");
+
+  statusbutton.innerText = book.readstatus;
+  removebutton.innerText = "Remove";
+
+  bookBoxButtons.append(statusbutton, removebutton);
+  bookContainer.appendChild(bookBox);
+  bookBox.append(firstP, secondP, thirdP);
+  bookBox.appendChild(bookBoxButtons);
+}
+
+
+function addBookToLibrary() {
+  myLibrary.forEach((book) => createBookElements(book));
+}
+
+addBookToLibrary()
